d3e0c35376f5a21f964397286c70562f5410d2f1
佐藤拓真 <takuma1229tennis@gmail.com>
2021-10-06 14:14:57 +0900

    solve 221c

--- abc_c/221.py
+++ abc_c/221.py
@@ -1,6 +1,19 @@
-import math
+import itertools as iter
 
 n = input()
 n_li = list(n)
 
-#順列全探索の問題や
+all_li = list(iter.permutations(n_li))
+
+ab_li = []
+
+for i in range(len(all_li)):
+  for k in range(1,len(n)):
+    a_li = all_li[i][:k]
+    a = "".join(a_li)
+    b_li = all_li[i][k:]
+    b = "".join(b_li)
+    ab = int(a) * int(b)
+    ab_li.append(ab)
+
+print(max(ab_li))
