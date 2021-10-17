7afbbd0cc7b228e21e6b41c50960ea9524bd8359
佐藤拓真 <takuma1229tennis@gmail.com>
2021-10-17 19:15:54 +0900

    refact 115c

--- abc_c/115.py
+++ abc_c/115.py
@@ -1,44 +1,11 @@
-def merge_sort(nums):
-    if len(nums) <= 1:
-        return nums
-
-    split = len(nums) // 2
-    left = nums[:split]
-    right = nums[split:]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    i, j, k = 0, 0, 0
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            nums[k] = left[i]
-            i += 1
-        else:
-            nums[k] = right[j]
-            j += 1
-        k += 1
-
-    while i < len(left):
-        nums[k] = left[i]
-        i += 1
-        k += 1
-
-    while j < len(right):
-        nums[k] = right[j]
-        j += 1
-        k += 1
-
-    return nums
-
-
 n, k = map(int, input().split())
 h_li = []
 
 for i in range(n):
     h_li.append(int(input()))
 
-h_s = merge_sort(h_li)
+# h_s = merge_sort(h_li)
+h_s = sorted(h_li)
 
 ans = 100000000000000000
 
