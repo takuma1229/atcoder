088b3a6c05aec436ede665eccfb980155aac7fb8
佐藤拓真 <takuma1229tennis@gmail.com>
2021-10-24 22:54:43 +0900

    solve 206c

--- abc_c/206.py
+++ abc_c/206.py
@@ -1,13 +1,34 @@
-from collections import Counter
-
-n = int(input())
+N = int(input())
 A = list(map(int, input().split()))
-c = Counter()
+count = 0
 
-ans = 0
-for i in range(n):
-  ans += i - c[A[i]]
-  c[A[i]] += 1
+A_s = sorted(A)
+A_s_u = list(set(A_s))
 
-print(ans)
+if len(A_s_u) == 1:
+    print(0)
+    exit()
 
+succession = 0
+
+for i in range(1, N):
+    if i == N-1:
+        if succession != 0:
+            if A_s[i] == A_s[i-1]:
+                succession += 1
+                count += (succession * (succession + 1)) / 2
+                break
+            else:
+                count += (succession * (succession + 1)) / 2
+                break
+    if A_s[i] == A_s[i-1]:
+        succession += 1
+    else:
+        if succession == 0:
+            continue
+        else:
+            count += (succession * (succession + 1)) / 2
+            succession = 0
+
+
+print(int(((N * (N-1))/2)-count))
