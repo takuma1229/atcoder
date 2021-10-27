d85d22f0b0798806287ada279e01ceaf6dc68183
佐藤拓真 <takuma1229tennis@gmail.com>
2021-10-27 23:29:06 +0900

    solve abc177c

--- abc_c/177.py
+++ abc_c/177.py
@@ -1,4 +1,15 @@
-n = int(input())
+N = int(input())
 A = list(map(int, input().split()))
+ans = 0
 
-print(((sum(A) * 2) - 1) % ((10 ** 9) + 7))
+li = [0] * N
+li[0] = A[0]
+
+for i in range(1, N):
+    li[i] = li[i-1] + A[i]
+
+for i in range(1, N):
+    ans += li[i-1] * A[i]
+    ans %= (10 ** 9 + 7)
+
+print(ans)
