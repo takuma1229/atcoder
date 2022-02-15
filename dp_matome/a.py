b6acd984d2efd148ed191c8fa2042f61336aa46d
佐藤拓真 <takuma1229tennis@gmail.com>
2022-02-15 23:23:29 +0900

    solve dpmatome a

--- dp_matome/a.py
+++ dp_matome/a.py
@@ -1,15 +1,12 @@
 N = int(input())
 h = list(map(int, input().split()))
 
-dp = [0] * N
+dp = [10**10] * N
 
 dp[0] = 0
-dp[1] = abs(h[1] - h[0])
+dp[1] = abs(h[1]-h[0])
 
 for i in range(2, N):
-    # i を現在いる足場と考える。
-    # i 番目の足場へ行く方法として i-i 番目からのジャンプと i-2 番目からのジャンプがある
-    # 2 通りの行き方のうちコストの少ない方を dp[i] とする
-    dp[i] = min(dp[i-2] + abs(h[i] - h[i-2]), dp[i-1] + abs(h[i] - h[i-1]))
+    dp[i] = min(abs(h[i]-h[i-1])+dp[i-1], abs(h[i]-h[i-2])+dp[i-2])
 
-print(dp[-1])
+print(dp[N-1])
